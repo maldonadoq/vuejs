68b0dcae2b7a1b6ef62dff92530b5acb49613fc4
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2020-01-28 21:07:55 -0500

    intro

---
+++ Intro/index.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    title: 'Hello from Vuejs',
+    fruits: [
+      {name: 'Apple', count: 10},
+      {name: 'Orange', count: 0},
+      {name: 'Banana', count: 23}
+    ]
+  }
+});
